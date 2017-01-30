9755e4fc5153697437912042d66e387fc0d8cd53
Bexhion <bexhionnxg@gmail.com>
2017-01-30 16:31:53 +0000

    Automatic form title

--- binary-chaos/src/Form1.Designer.vb
+++ binary-chaos/src/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class Window_main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,30 +22,16 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
-        '
-        'Form1
+        'Window_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(374, 283)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "Window_main"
+        Me.Text = "Filler"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
 End Class
--- binary-chaos/src/Form1.vb
+++ binary-chaos/src/Form1.vb
@@ -1,8 +1,12 @@
-﻿Public Class Form1
+﻿Public Class Window_main
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim width As Integer = Screen.PrimaryScreen.Bounds.Width
         Dim height As Integer = Screen.PrimaryScreen.Bounds.Height
         Me.Size = New Size(width * 0.75, height * 0.75)
         Me.Location = New Point(width * 0.125, height * 0.125)
+    End Sub
+
+    Public Sub changeWindowTitle(text As String)
+        Me.Text = text
     End Sub
 End Class
